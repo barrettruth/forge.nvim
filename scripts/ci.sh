0c41ff01fb20724abfef9d996bb6cf8ece17ff5d
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-28 00:52:19 -0400

    ci: script

---
+++ scripts/ci.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+nix develop .#ci --command stylua --check .
+git ls-files '*.lua' | xargs nix develop .#ci --command selene --display-style quiet
+nix develop .#ci --command prettier --check .
+nix fmt -- --check .
+nix develop .#ci --command lua-language-server --check lua --configpath "$(pwd)/.luarc.json" --checklevel=Warning
+nix develop .#ci --command vimdoc-language-server check doc/
